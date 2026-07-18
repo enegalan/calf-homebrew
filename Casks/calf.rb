dacde55b0a418f2dd722a4690a18aa0ddb4c0040
Eneko Galan <enekogalanelorza@gmail.com>
2026-07-19 00:24:07 +0200

    chore: update calf cask to version 0.9.4 with new sha256 checksum

--- Casks/calf.rb
+++ Casks/calf.rb
@@ -1,6 +1,6 @@
 cask "calf" do
-  version "0.9.3"
-  sha256 "c50b0c7bf4a3f3e5b5975cc4e8558bf53d1bd0336e5b592b4c5e42b0410e6fde"
+  version "0.9.4"
+  sha256 "f2cbfbe7e6097a8f92a17c2e916c023312ebdb0a7332db2fd749786e525c5023"
 
   url "https://github.com/enegalan/calf/releases/download/v#{version}/Calf-#{version}.dmg"
   name "Calf"
