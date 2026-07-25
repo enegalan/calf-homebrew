4a93d8978a15efb511132d0adb3b124d82d474cb
Eneko Galan <enekogalanelorza@gmail.com>
2026-07-25 03:40:25 +0200

    chore: update calf cask to version 0.9.9 with new sha256 checksum and corrected app naming

--- Casks/calf.rb
+++ Casks/calf.rb
@@ -1,19 +1,19 @@
 cask "calf" do
-  version "0.9.8"
-  sha256 "a4d7f21790e58c599c4f9c4a04205a280bc5567cd45f9d9f2f351dbcfd4e3f99"
+  version "0.9.9"
+  sha256 "a34856cf22d59edc860a31f6fa3c7dc46963c40bcdc11fe10218798c0f674889"
 
-  url "https://github.com/enegalan/calf/releases/download/v#{version}/Calf-#{version}.dmg"
-  name "Calf"
+  url "https://github.com/enegalan/calf/releases/download/v#{version}/calf-#{version}.dmg"
+  name "calf"
   desc "Lightweight, open-source alternative to Docker Desktop"
   homepage "https://github.com/enegalan/calf"
 
-  app "Calf.app"
+  app "calf.app"
 
   postflight do
     if OS.mac?
       system_command "/usr/bin/xattr",
-                     args: ["-dr", "com.apple.quarantine", "#{appdir}/Calf.app"],
-                     sudo: false
+                    args: ["-dr", "com.apple.quarantine", "#{appdir}/calf.app"],
+                    sudo: false
     end
   end
 
